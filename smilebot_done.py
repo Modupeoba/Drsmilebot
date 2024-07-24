2d3c5b8413d7b9e79be1d4fdb0a060d634aa0fb2
Modupeoba <162997237+Modupeoba@users.noreply.github.com>
2024-07-24 20:54:19 +0100

    Update smilebot_done.py

--- smilebot_done.py
+++ smilebot_done.py
@@ -44,14 +44,15 @@ from google.generativeai import configure
 
 # Retrieve Gemini API key from Google Colab's userdata
 # GEMINI_API_KEY = 'AIzaSyB-3SICe5or9W7Dv3X0-8yaZqS1ForMd0k'
-GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+
+os.environ["GEMINI_API_KEY"] = st.secrets["GEMINI_API_KEY"]
 # Configure the Gemini API
 configure(api_key=GEMINI_API_KEY)  # configure the API
 
-os.environ["AIzaSyB-3SICe5or9W7Dv3X0-8yaZqS1ForMd0k"] = GEMINI_API_KEY
+# os.environ["AIzaSyB-3SICe5or9W7Dv3X0-8yaZqS1ForMd0k"] = GEMINI_API_KEY
 
-GOOGLE_API_KEY = 'AIzaSyB-3SICe5or9W7Dv3X0-8yaZqS1ForMd0k'
-os.environ["GOOGLE_API_KEY"] = st.secrets[GOOGLE_API_KEY]
+# GOOGLE_API_KEY = 'AIzaSyB-3SICe5or9W7Dv3X0-8yaZqS1ForMd0k'
+# os.environ["GOOGLE_API_KEY"] = st.secrets[GOOGLE_API_KEY]
 
 import google.generativeai as genai
 from langchain.embeddings import GooglePalmEmbeddings
