69cc0a2cfb59725369b9b2143efaeddff7e55665
Modupeoba <162997237+Modupeoba@users.noreply.github.com>
2024-07-25 03:39:54 +0100

    Update smilebot_done.py

--- smilebot_done.py
+++ smilebot_done.py
@@ -129,7 +129,7 @@ def main():
     
     # Adding space before the image
     st.markdown("<div style='text-align: center; margin-top: 20px;'>", unsafe_allow_html=True)
-    st.image(width=800, "teeth-dental-care-medical-background.png")  # Replace with your main image path
+    st.image("teeth-dental-care-medical-background.png")  # Replace with your main image path
     st.markdown("</div>", unsafe_allow_html=True)
     
     # Sidebar options
